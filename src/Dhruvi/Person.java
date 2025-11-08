f166043803aeef7e86d88ac4fc6ff463d03b0114
Dhruvi Mittal <dhruvimittal2066@gmail.com>
2025-11-09 02:43:26 +0530

    Create Person.java

---
+++ src/Dhruvi/Person.java
@@ -0,0 +1,35 @@
+/*
+ * Person.java - Base class for Student Management System
+ * Author: Dhruvi
+ * Date: November 2025
+ */
+
+public class Person {
+    protected String name;
+    protected int age;
+    
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public int getAge() {
+        return age;
+    }
+    
+    public void setAge(int age) {
+        this.age = age;
+    }
+    
+    public void displayInfo() {
+        System.out.println("Name: " + name + ", Age: " + age);
+    }
+}
